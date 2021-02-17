e4c9397a9c37703af9a72e858efa1bf95ec01fc5
Maria <Maria@ERIZO_NB>
2021-02-17 22:08:40 +0300

    start task 1

---
+++ NumericUpDownLight/FormApp/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace FormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownLight1 = new NumericUpDownLight.NumericUpDownLight();
+            this.SuspendLayout();
+            // 
+            // numericUpDownLight1
+            // 
+            this.numericUpDownLight1.Location = new System.Drawing.Point(349, 65);
+            this.numericUpDownLight1.Name = "numericUpDownLight1";
+            this.numericUpDownLight1.Size = new System.Drawing.Size(75, 23);
+            this.numericUpDownLight1.TabIndex = 0;
+            this.numericUpDownLight1.Text = "numericUpDownLight1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDownLight1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NumericUpDownLight.NumericUpDownLight numericUpDownLight1;
+    }
+}
+
---
+++ NumericUpDownLight/NumericUpDownLight/NumericUpDownLight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericUpDownLight
+{
+    public class NumericUpDownLight: System.Windows.Forms.Control
+    {
+    }
+}
